2d0e47d2ed064df7c8bae71977814582a0367ed2
Eduardo Cabral <eduardosbcabral@gmail.com>
2023-09-25 14:23:51 -0300

    feat: adds automapper

---
+++ src/Scaffolding/Extensions/Mapper/MapperServiceExtension.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using System.Reflection;
+
+namespace Scaffolding.Extensions.Mapper
+{
+    public static class MapperServiceExtension
+    {
+        public static void SetupAutoMapper(this WebApplicationBuilder builder, params Assembly[] assemblies)
+        {
+            builder.Services.AddAutoMapper(assemblies);
+        }
+
+        public static void UseAutoMapper(this WebApplication app)
+        {
+            var mapper = app.Services.GetService<IMapper>();
+            GlobalMapper.Mapper = mapper;
+        }
+    }
+}
