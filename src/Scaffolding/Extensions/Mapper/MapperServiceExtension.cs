d0d749cc0db4b17b29f439e8686abbace0794b31
Eduardo Cabral <eduardosbcabral@gmail.com>
2023-09-26 11:21:43 -0300

    fix: improves AutoMapper setup

--- src/Scaffolding/Extensions/Mapper/MapperServiceExtension.cs
+++ src/Scaffolding/Extensions/Mapper/MapperServiceExtension.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.Extensions.DependencyModel;
 using System.Reflection;
 
 namespace Scaffolding.Extensions.Mapper
@@ -7,13 +8,31 @@ namespace Scaffolding.Extensions.Mapper
     {
         public static void SetupAutoMapper(this WebApplicationBuilder builder, params Assembly[] assemblies)
         {
-            builder.Services.AddAutoMapper(assemblies);
+            builder.Services.AddAutoMapper(GetAssemblies(assemblies));
         }
 
         public static void UseAutoMapper(this WebApplication app)
         {
             var mapper = app.Services.GetService<IMapper>();
             GlobalMapper.Mapper = mapper;
+        }
+
+        private static Assembly[] GetAssemblies(Assembly[] customAssemblies)
+        {
+            var assemblies = new List<Assembly>();
+            var dependencies = DependencyContext.Default.RuntimeLibraries.Where(p =>
+                p.Type.Equals("Project", StringComparison.CurrentCultureIgnoreCase));
+
+            foreach (var library in dependencies)
+            {
+                var name = new AssemblyName(library.Name);
+                var assembly = Assembly.Load(name);
+                assemblies.Add(assembly);
+            }
+
+            assemblies.AddRange(customAssemblies);
+
+            return assemblies.ToArray();
         }
     }
 }
